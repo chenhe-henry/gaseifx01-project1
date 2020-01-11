244de394dba01c072204152703f6e71ca97e54a3
chenhe-henry <chenhe.elec@gmail.com>
2020-01-11 13:44:55 +1100

    commented dependencies take 1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,8 @@ require("channels");
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require chartkick
+//= require Chart.bundle
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,6 +26,3 @@ require("channels");
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require("chartkick");
-require("chart.js");
-require("Chart.bundle.js");
