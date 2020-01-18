6016c94028ef8bc67bb3f283966500506d285169
chenhe-henry <chenhe.elec@gmail.com>
2020-01-18 21:39:49 +1100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_195627) do
+ActiveRecord::Schema.define(version: 2019_10_30_102845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,16 +46,10 @@ ActiveRecord::Schema.define(version: 2019_11_01_195627) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "genders", force: :cascade do |t|
-    t.text "gender"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.integer "gender_id"
+    t.string "gender"
     t.string "city"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
